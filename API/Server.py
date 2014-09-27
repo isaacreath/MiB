7ce1941b54e48443e6f01c3c3e4779dc6c3e2ad0
Isaac Reath <isaacreath@dhcp-ccc-11916.redrover.cornell.edu>
2014-09-27 15:11:11 -0400

    documented drop endpoint

--- API/Server.py
+++ API/Server.py
@@ -9,6 +9,8 @@ db = client['messages']
 collection = db['collection']
 posts = db.posts
 
+
+
 def validate_post_request(args):
     for arg in args:
         if arg is None:
@@ -16,18 +18,33 @@ def validate_post_request(args):
     return True
 
 
+
+'''
+Endpoint for adding a new bottle to the database. Requires 5 arguments
+x: the x coordinate where the bottle is dropped (float)
+y: the y coordinate where the bottle is dropped (float)
+message: the content of the message  (plain text, or matrix representation of picture/video)
+userId: the unique user id of the user who dropped the message (int)
+viewable_by: a string containing who can view the message (friend or public)
+
+'''
+
 @app.route("/dropMessage", methods=['POST'])
 def drop_message():
     message_x = request.form.get('x')
     message_y = request.form.get('y')
     message = request.form.get('message')
     user_id = request.form.get('userId')
-    args = [message_x, message_y, message, user_id]
+    viewable_by = request.form.get('viewableBy')
+
+    args = [message_x, message_y, message, user_id, visability]
     if validate_post_request(args):
         message_coordinates = (message_x, message_y)
         print "(" + str(message_coordinates) + ") " + str(message)
-        message_entry = {"coordinates" : message_coordinates,
-                         "message":message
+        message_entry = {"coordinates":message_coordinates,
+                         "message":message,
+                         "userid":user_id,
+                         "viewable_by":viewable_by
                         }
         posts.insert(message_entry)
         for p in posts.find():
