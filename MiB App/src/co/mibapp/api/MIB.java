887cf7b15067cd5d7b414dbfc038a271a6ec7d7b
Gino <ginonott@buffalo.edu>
2014-09-27 17:25:39 -0400

    Refactored project structure

---
+++ MiB App/src/co/mibapp/api/MIB.java
@@ -0,0 +1,7 @@
+package co.mibapp.api;
+
+/**
+ * Created by Gino on 9/27/2014.
+ */
+public interface MIB {
+}
---
+++ MiB App/src/co/mibapp/api/MIBImplementation.java
@@ -0,0 +1,8 @@
+package co.mibapp.api;
+
+/**
+ * Created by Gino on 9/27/2014.
+ */
+public class MIBImplementation {
+
+}
