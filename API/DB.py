25fc20331a88d007952e696f5dafdc6df197fbee
Isaac Reath <isaacreath@dhcp-ccc-11916.redrover.cornell.edu>
2014-09-27 15:45:13 -0400

    added friend/user databases

--- API/DB.py
+++ API/DB.py
@@ -4,8 +4,12 @@ class Database():
     def __init__(self):
         self.client = MongoClient()
         self.db = self.client['messages']
+        self.friends_database = self.client['friends']
+        self.user_database = self.client['users']
         self.collection = self.db['collection']
         self.posts = self.db.posts
+        self.user_posts = self.user_database.posts
+        self.friends_posts = self.friends_database.posts
 
     def insert(self, entry):
         self.posts.insert(entry)
@@ -14,4 +18,8 @@ class Database():
         for p in self.posts.find():
             print p
 
+    def add_user(self, user):
+        self.user_posts.insert(user)
 
+    def add_friend(self, friend):
+        self.friends_posts.insert(friend)
