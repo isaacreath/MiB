d1629efc2b39e1e094c5e942075a1e87eb30f7c4
Isaac Reath <isaacreath@dhcp-ccc-11916.redrover.cornell.edu>
2014-09-27 03:47:20 -0400

    added initial message drop endpoint

---
+++ API/Server.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from pymongo import MongoClient
+from flask import request
+
+#setup the app and the db
+app = Flask(__name__)
+client = MongoClient()
+db = client['messages']
+collection = db['collection']
+posts = db.posts
+
+
+@app.route("/dropMessage", methods=['POST'])
+def drop_message():
+    print "ASDFA"
+    message_coordinates = request.form.get('coords')
+    message = request.form.get('message')
+    print "(" + str(message_coordinates) + ") " + str(message)
+    message_entry = {"coordinates" : message_coordinates,
+                     "message":message
+                    }
+    posts.insert(message_entry)
+    for p in posts.find():
+        print p
+
+    return "Post Successful"
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
